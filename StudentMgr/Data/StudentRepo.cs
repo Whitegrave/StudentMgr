90597ea5b749e6e0837f47ce50bff74fe81d08d2
Whitegrave <mrcraig0907@gmail.com>
2021-06-10 13:02:32 -0400

    Add project files.

---
+++ StudentMgr/Data/StudentRepo.cs
@@ -0,0 +1,33 @@
+﻿using StudentMgr.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentMgr.Data
+{
+    public class StudentRepo
+    {
+        // List, add, edit, delete
+        public List<Student> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Student toAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Student toEdit, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete (int toDelete)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
